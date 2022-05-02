c3d2e7a1efb3d6ae15789268432c286a55fa51b4
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-05-01 22:57:19 -0400

    fixed route

--- src/main/constants.js
+++ src/main/constants.js
@@ -1,6 +1,6 @@
 const exported = {
-  // API_BACKEND: "https://vast-meadow-87338.herokuapp.com"
-  API_BACKEND: "http://localhost:8000",
+  API_BACKEND: "https://vast-meadow-87338.herokuapp.com",
+  // API_BACKEND: "http://localhost:8000",
 };
 
 export default exported;
